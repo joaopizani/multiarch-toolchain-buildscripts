49b38cf068df149c69e3765e2879c406379f8e9a
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-08-12 23:12:39 -0300

    new gcc and gdb versions in the AVR toolchain and suggestion of an ARM Zynq buildscript

--- build_arm_zynq_toolchain.sh
+++ build_arm_zynq_toolchain.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# ARM Xilinx Zynq toolchain buildscript
+#
 # Toolchain buildscript
 # This script requires TWO command-line parameters: the number of threads for make and the
 # directory under which all tools are going to be installed.
 
 # Before running the script, the following packages need to be installed in your distro:
-# (These are Ubuntu package names, if you don't use Ubuntu, look for equivalents)
+# (These are Ubuntu package names, if y,ou don't use Ubuntu, look for equivalents)
 #
 # build-essential, automake, autoconf, m4, flex, bison, texinfo
 
@@ -16,7 +18,7 @@ BUILD_HOME=$2   # DON'T CHANGE
 SILENT_BUILD=
 
 # use the GCC target name for the desired architecture here, i.e, "avr", "mips-elf", etc.
-TARGET=avr
+TARGET=arm-xilinx-linux-gnueabi
 
 # You should probably use the most recent stable versions... but sometimes you need
 # a bit more research...
@@ -25,10 +27,8 @@ GMP_VERSION=5.0.4
 MPFR_VERSION=3.1.0
 MPC_VERSION=0.9
 GCC_VERSION=4.7.1
-AVRLIBC_VERSION=1.8.0
-SIMULAVR_VERSION=1.0.0
+GLIBC_VERSION=2.16.0
 GDB_VERSION=7.4.1
-AVRDUDE_VERSION=5.11.1
 
 
 # Optional editing: only if some site has changed
@@ -37,10 +37,8 @@ GMP_SITE=ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION
 MPFR_SITE=http://mpfr.loria.fr/mpfr-current
 MPC_SITE=http://www.multiprecision.org/mpc/download
 GCC_SITE=http://gnu.c3sl.ufpr.br/ftp/gcc/gcc-$GCC_VERSION
-AVRLIBC_SITE=http://download.savannah.gnu.org/releases/avr-libc
-SIMULAVR_SITE=http://download.savannah.gnu.org/releases/simulavr
+GLIBC_SITE=http://ftp.gnu.org/gnu/glibc
 GDB_SITE=http://ftp.gnu.org/gnu/gdb
-AVRDUDE_SITE=http://savannah.c3sl.ufpr.br/avrdude
 
 
 #### CAREFUL EDITING FROM HERE ON ####
@@ -53,29 +51,26 @@ GMP=gmp-$GMP_VERSION
 MPFR=mpfr-$MPFR_VERSION
 MPC=mpc-$MPC_VERSION
 GCC=gcc-$GCC_VERSION
-AVRLIBC=avr-libc-$AVRLIBC_VERSION
-SIMULAVR=simulavr-$SIMULAVR_VERSION
+GLIBC=glibc-$GLIBC_VERSION
 GDB=gdb-$GDB_VERSION
-AVRDUDE=avrdude-$AVRDUDE_VERSION
 
 
-PACKLITERALS=(  '$BINUTILS' '$GMP'     '$MPFR'   '$MPC'    '$GCC'    '$AVRLIBC' '$SIMULAVR' '$GDB'    '$AVRDUDE')
-PACKEXTENSIONS=('.tar.gz'   '.tar.bz2' '.tar.gz' '.tar.gz' '.tar.gz' '.tar.bz2' '.tar.gz'   '.tar.gz' '.tar.gz')
-TASKLITERALS='$BINUTILS $GMP $MPFR $MPC $GCC $AVRLIBC $GCC $SIMULAVR $GDB $AVRDUDE' # 2 GCC tasks (bootstrap)
+PACKLITERALS=(  '$BINUTILS' '$GMP'     '$MPFR'   '$MPC'    '$GCC'      '$GLIBC'   '$GDB')
+PACKEXTENSIONS=('.tar.gz'   '.tar.bz2' '.tar.gz' '.tar.gz' '.tar.gz'   '.tar.gz'  '.tar.gz')
+TASKLITERALS='$BINUTILS $GMP $MPFR $MPC $GCC $GLIBC $GCC $GDB' # 2 GCC tasks (pre- and post-bootstrap)
 
 # Configure options for all packages. Change only if the "reasonable" defaults don't work
-BINUTILS_OPT="--target=$TARGET --disable-nls --disable-shared --disable-threads --with-gcc --with-gnu-as --with-gnu-ld --enable-install-libbfd --enable-install-libiberty --disable-werror"
+BINUTILS_OPT="--target=$TARGET --disable-nls --disable-shared --with-gnu-as --with-gnu-ld --enable-install-libbfd --disable-werror"
 GMP_OPT=""
 MPFR_OPT="--with-gmp=$PREFIX"
 MPC_OPT="--with-gmp=$PREFIX --with-mpfr=$PREFIX"
-GCC_OPT="--target=$TARGET --with-mpfr=$PREFIX --with-mpc=$PREFIX --disable-nls --enable-languages=c --without-headers --with-multilib --disable-libssp --disable-shared --disable-threads"
-AVRLIBC_OPT="--host=$TARGET"
-GCC_2_OPT="--target=$TARGET --with-mpfr=$PREFIX --with-mpc=$PREFIX --disable-nls --enable-languages=c,c++ --with-multilib --disable-libssp --disable-shared --disable-threads"
-SIMULAVR_OPT="--with-bfd=$PREFIX/i686-pc-linux-gnu/avr --with-libiberty=$PREFIX/lib CFLAGS=-Wno-error"
+GCC_OPT="--target=$TARGET --with-mpfr=$PREFIX --with-mpc=$PREFIX --disable-nls --enable-languages=c --without-headers --with-newlib --with-multilib --disable-libssp --disable-shared --disable-threads"
+GLIBC_OPT="--host=$TARGET"
+GCC_2_OPT="--target=$TARGET --with-mpfr=$PREFIX --with-mpc=$PREFIX --disable-nls --enable-languages=c,c++ --with-newlib --with-multilib --disable-libssp --disable-shared --disable-threads"
 GDB_OPT="--target=$TARGET --with-gmp=$PREFIX --with-mpfr=$PREFIX --disable-nls --disable-libssp --disable-werror"
-AVRDUDE_OPT="--target=$TARGET"
 
 
 
-#### After defining all necessary variables, source the generic build script now ####
+
+#### After defining all necessary variables, source the generic part now ####
 source ./build_toolchain.sh
