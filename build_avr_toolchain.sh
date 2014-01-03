b5a6755aaca0573ca3cb4e5522c0697ddd46ce2b
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-01-03 16:28:07 -0200

    updating quoting and versions etc

--- build_avr_toolchain.sh
+++ build_avr_toolchain.sh
@@ -8,60 +8,60 @@
 #
 # build-essential, automake, autoconf, m4, flex, bison, texinfo
 
-THREADS="-j $1" # DON'T CHANGE
-BUILD_HOME=$2   # DON'T CHANGE
+THREADS="-j $1"  # DON'T CHANGE
+BUILD_HOME="$2"  # DON'T CHANGE
 
 
 # if you want less messages from compilation in your output, use SILENT_BUILD=1
 SILENT_BUILD=
 
 # use the GCC target name for the desired architecture here, i.e, "avr", "mips-elf", etc.
-TARGET=avr
+TARGET="avr"
 
 # You should probably use the most recent stable versions... but sometimes you need
 # a bit more research...
-BINUTILS_VERSION=2.22
-GMP_VERSION=5.0.4
-MPFR_VERSION=3.1.0
-MPC_VERSION=0.9
-GCC_VERSION=4.7.1
-AVRLIBC_VERSION=1.8.0
-SIMULAVR_VERSION=1.0.0
-GDB_VERSION=7.4.1
-AVRDUDE_VERSION=5.11.1
+BINUTILS_VERSION="2.24"
+GMP_VERSION="5.1.3"
+MPFR_VERSION="3.1.2"
+MPC_VERSION="1.0.1"
+GCC_VERSION="4.8.2"
+AVRLIBC_VERSION="1.8.0"
+SIMULAVR_VERSION="1.0.0"
+GDB_VERSION="7.6.2"
+AVRDUDE_VERSION="6.0.1"
 
 
 # Optional editing: only if some site has changed
-BINUTILS_SITE=http://ftp.gnu.org/gnu/binutils
-GMP_SITE=ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION
-MPFR_SITE=http://mpfr.loria.fr/mpfr-current
-MPC_SITE=http://www.multiprecision.org/mpc/download
-GCC_SITE=http://gnu.c3sl.ufpr.br/ftp/gcc/gcc-$GCC_VERSION
-AVRLIBC_SITE=http://download.savannah.gnu.org/releases/avr-libc
-SIMULAVR_SITE=http://download.savannah.gnu.org/releases/simulavr
-GDB_SITE=http://ftp.gnu.org/gnu/gdb
-AVRDUDE_SITE=http://savannah.c3sl.ufpr.br/avrdude
+BINUTILS_SITE="http://ftp.gnu.org/gnu/binutils"
+GMP_SITE="ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION"
+MPFR_SITE="http://www.mpfr.org/mpfr-current"
+MPC_SITE="http://www.multiprecision.org/mpc/download"
+GCC_SITE="http://gnu.c3sl.ufpr.br/ftp/gcc/gcc-$GCC_VERSION"
+AVRLIBC_SITE="http://download.savannah.gnu.org/releases/avr-libc"
+SIMULAVR_SITE="http://download.savannah.gnu.org/releases/simulavr"
+GDB_SITE="http://ftp.gnu.org/gnu/gdb"
+AVRDUDE_SITE="http://savannah.c3sl.ufpr.br/avrdude"
 
 
 #### CAREFUL EDITING FROM HERE ON ####
 
 
-PREFIX=$BUILD_HOME/$TARGET/gcc-$GCC_VERSION
+PREFIX="$BUILD_HOME/$TARGET/gcc-$GCC_VERSION"
 
-BINUTILS=binutils-$BINUTILS_VERSION
-GMP=gmp-$GMP_VERSION
-MPFR=mpfr-$MPFR_VERSION
-MPC=mpc-$MPC_VERSION
-GCC=gcc-$GCC_VERSION
-AVRLIBC=avr-libc-$AVRLIBC_VERSION
-SIMULAVR=simulavr-$SIMULAVR_VERSION
-GDB=gdb-$GDB_VERSION
-AVRDUDE=avrdude-$AVRDUDE_VERSION
+BINUTILS="binutils-$BINUTILS_VERSION"
+GMP="gmp-$GMP_VERSION"
+MPFR="mpfr-$MPFR_VERSION"
+MPC="mpc-$MPC_VERSION"
+GCC="gcc-$GCC_VERSION"
+AVRLIBC="avr-libc-$AVRLIBC_VERSION"
+SIMULAVR="simulavr-$SIMULAVR_VERSION"
+GDB="gdb-$GDB_VERSION"
+AVRDUDE="avrdude-$AVRDUDE_VERSION"
 
 
 PACKLITERALS=(  '$BINUTILS' '$GMP'     '$MPFR'   '$MPC'    '$GCC'    '$AVRLIBC' '$SIMULAVR' '$GDB'    '$AVRDUDE')
 PACKEXTENSIONS=('.tar.gz'   '.tar.bz2' '.tar.gz' '.tar.gz' '.tar.gz' '.tar.bz2' '.tar.gz'   '.tar.gz' '.tar.gz')
-TASKLITERALS='$BINUTILS $GMP $MPFR $MPC $GCC $AVRLIBC $GCC $SIMULAVR $GDB $AVRDUDE' # 2 GCC tasks (bootstrap)
+TASKLITERALS=( '$BINUTILS' '$GMP'      '$MPFR'  '$MPC'  '$GCC' '$AVRLIBC' '$GCC' '$SIMULAVR' '$GDB' '$AVRDUDE')
 
 # Configure options for all packages. Change only if the "reasonable" defaults don't work
 BINUTILS_OPT="--target=$TARGET --disable-nls --disable-shared --disable-threads --with-gcc --with-gnu-as --with-gnu-ld --enable-install-libbfd --enable-install-libiberty --disable-werror"
--- build_template_toolchain.sh
+++ build_template_toolchain.sh
@@ -11,53 +11,52 @@
 # build-essential, automake, autoconf, m4, flex, bison, texinfo
 
 THREADS="-j $1" # DON'T CHANGE
-BUILD_HOME=$2   # DON'T CHANGE
+BUILD_HOME="$2"   # DON'T CHANGE
 
 
 # if you want less messages from compilation in your output, use SILENT_BUILD=1
 SILENT_BUILD=
 
 # use the GCC target name for the desired architecture here, i.e, "avr", "mips-elf", etc.
-TARGET=
+TARGET=""
 
-# You should probably use the most recent stable versions... but sometimes you need
-# a bit more research...
-BINUTILS_VERSION=
-GMP_VERSION=
-MPFR_VERSION=
-MPC_VERSION=
-GCC_VERSION=
-NEWLIB_VERSION=
-GDB_VERSION=
+# You should probably use the most recent stable versions... but sometimes you need more research...
+BINUTILS_VERSION=""
+GMP_VERSION=""
+MPFR_VERSION=""
+MPC_VERSION=""
+GCC_VERSION=""
+NEWLIB_VERSION=""
+GDB_VERSION=""
 
 
 # Optional editing: only if some site has changed
-BINUTILS_SITE=http://ftp.gnu.org/gnu/binutils
-GMP_SITE=ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION
-MPFR_SITE=http://mpfr.loria.fr/mpfr-current
-MPC_SITE=http://www.multiprecision.org/mpc/download
-GCC_SITE=http://gnu.c3sl.ufpr.br/ftp/gcc/gcc-$GCC_VERSION
-NEWLIB_SITE=ftp://sources.redhat.com/pub/newlib
-GDB_SITE=http://ftp.gnu.org/gnu/gdb
+BINUTILS_SITE="http://ftp.gnu.org/gnu/binutils"
+GMP_SITE="ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION"
+MPFR_SITE="http://mpfr.loria.fr/mpfr-current"
+MPC_SITE="http://www.multiprecision.org/mpc/download"
+GCC_SITE="http://gnu.c3sl.ufpr.br/ftp/gcc/gcc-$GCC_VERSION"
+NEWLIB_SITE="ftp://sources.redhat.com/pub/newlib"
+GDB_SITE="http://ftp.gnu.org/gnu/gdb"
 
 
 #### CAREFUL EDITING FROM HERE ON ####
 
 
-PREFIX=$BUILD_HOME/$TARGET/gcc-$GCC_VERSION
+PREFIX="$BUILD_HOME/$TARGET/gcc-$GCC_VERSION"
 
-BINUTILS=binutils-$BINUTILS_VERSION
-GMP=gmp-$GMP_VERSION
-MPFR=mpfr-$MPFR_VERSION
-MPC=mpc-$MPC_VERSION
-GCC=gcc-$GCC_VERSION
-NEWLIB=newlib-$NEWLIB_VERSION
-GDB=gdb-$GDB_VERSION
+BINUTILS="binutils-$BINUTILS_VERSION"
+GMP="gmp-$GMP_VERSION"
+MPFR="mpfr-$MPFR_VERSION"
+MPC="mpc-$MPC_VERSION"
+GCC="gcc-$GCC_VERSION"
+NEWLIB="newlib-$NEWLIB_VERSION"
+GDB="gdb-$GDB_VERSION"
 
 
-PACKLITERALS=(  '$BINUTILS' '$GMP'     '$MPFR'   '$MPC'    '$GCC'    '$NEWLIB' '$GDB')
-PACKEXTENSIONS=('.tar.gz'   '.tar.bz2' '.tar.gz' '.tar.gz' '.tar.gz' '.tar.gz' '.tar.gz')
-TASKLITERALS='$BINUTILS $GMP $MPFR $MPC $GCC $NEWLIB $GCC $GDB' # 2 GCC tasks (pre- and post-bootstrap)
+PACKLITERALS=(   '$BINUTILS'  '$GMP'      '$MPFR'    '$MPC'     '$GCC'     '$NEWLIB'   '$GDB')
+PACKEXTENSIONS=( '.tar.gz'    '.tar.bz2'  '.tar.gz'  '.tar.gz'  '.tar.gz'  '.tar.gz'   '.tar.gz')
+TASKLITERALS='   $BINUTILS    $GMP        $MPFR      $MPC       $GCC       $NEWLIB     $GCC  $GDB'
 
 # Configure options for all packages. Change only if the "reasonable" defaults don't work
 BINUTILS_OPT="--target=$TARGET --disable-nls --disable-shared --with-gnu-as --with-gnu-ld --enable-install-libbfd --disable-werror"
