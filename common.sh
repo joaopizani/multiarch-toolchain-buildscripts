6c03d7b3baa52369fba5816f0ece7a1163fe72e5
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-09-30 16:26:36 +0200

    tarring files to standardized target directories

--- common.sh
+++ common.sh
@@ -14,19 +14,21 @@ for k in ${!PACKS[@]}; do
 done
 
 # Unpack if necessary
-cd "${BUILDDIR}"
 for p in "${PACKS[@]}"; do
-    [ ! -d "${p%%.tar.*}" ] && { tar -xaf "${PACKDIR}/${p}" || { echo "Error unpacking ${p}"; exit 2; } }
+    if [ ! -d "${BUILDDIR}/${p%%.tar.*}" ]; then
+        mkdir -p "${BUILDDIR}/${p%%.tar.*}" || { echo "Error creating directory to unpack ${p}"; exit 2; }
+        tar -C "${BUILDDIR}/${p%%.tar.*}" --strip-components=1 -xaf "${PACKDIR}/${p}" || { echo "Error unpacking ${p}"; exit 3; }
+    fi
 done
 
 # Make build directories
-for p in "${PACKS[@]}"; do mkdir -p "build-${p%%.tar.*}"; done
+for p in "${PACKS[@]}"; do mkdir -p "${BUILDDIR}/build-${p%%.tar.*}"; done
 
 # Set-up some environment variables useful during the build process
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-export PATH="${PREFIX}/bin:${PATH}"
+export LD_LIBRARY_PATH="${PREFIX}/lib" PATH="${PREFIX}/bin:${PATH}"
 
 # configure, make and make install all packs
+cd "${BUILDDIR}"
 GCC_BOOTSTRAPPED=0
 for t in "${OPTS[@]}"; do
     cd "build-${PACKS[$t]%%.tar.*}"
@@ -34,13 +36,13 @@ for t in "${OPTS[@]}"; do
     if [ "${t}" = 'GCC' -a "${GCC_BOOTSTRAPPED}" = 1 ]; then  # GCC again (post-bootstrap)
         "../${PACKS[$t]%%.tar.*}/configure" --prefix="${PREFIX}" ${GCC2}
     else  [ ! -e Makefile ] && "../${PACKS[$t]%%.tar.*}/configure" --prefix="${PREFIX}" ${!t};  fi
-    [ $? != 0 ] && { echo "Error configuring ${PACKS[$t]}"; exit 3; }
+    [ $? != 0 ] && { echo "Error configuring ${PACKS[$t]}"; exit 4; }
 
     if [ ${t} = 'GCC' ]; then make -j${THR} all-gcc all-target-libgcc; else make -j${THR} all; fi
-    [ $? != 0 ] && { echo "Error making ${PACKS[$t]}"; exit 4; }
+    [ $? != 0 ] && { echo "Error making ${PACKS[$t]}"; exit 5; }
 
     if [ ${t} = 'GCC' ]; then make install-gcc install-target-libgcc; else make install; fi
-    [ $? != 0 ] && { echo "Error installing ${PACKS[$t]}"; exit 5; }
+    [ $? != 0 ] && { echo "Error installing ${PACKS[$t]}"; exit 6; }
     [ ${t} = 'GCC' ] && GCC_BOOTSTRAPPED=1
 
     cd ..
